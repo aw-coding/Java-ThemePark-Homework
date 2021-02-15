538277fb586c11a5e6877c1e49a5bd5a033e2147
Andrew Wood <andrewwood90@hotmail.com>
2021-02-15 18:15:24 +0000

    one playground test passed

---
+++ src/test/java/attractions/PlaygroundTest.java
@@ -0,0 +1,40 @@
+package attractions;
+
+import org.junit.Before;
+import org.junit.Test;
+import people.Visitor;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlaygroundTest {
+    Playground playground;
+    Visitor visitor1;
+    Visitor visitor2;
+
+    @Before
+    public void setUp() throws Exception {
+        visitor1 = new Visitor(20, 190.1, 300);
+        visitor2= new Visitor(10, 130.5, 30);
+        playground = new Playground("Fun Zone", 7);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Fun Zone", playground.getName());
+    }
+
+    @Test
+    public void hasRating() {
+        assertEquals(7, playground.getRating());
+    }
+
+    @Test
+    public void hasVisitCount() {
+        assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void canTestForAgeAndPass(){
+        assertEquals(true, playground.isAllowedTo(visitor1));
+    }
+}

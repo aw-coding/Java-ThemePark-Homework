4bcf5715ec5bfd375ab3b99e1436eda4b4558e09
Andrew Wood <andrewwood90@hotmail.com>
2021-02-15 18:53:32 +0000

    5 dodgems tests passed

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,8 +1,19 @@
 package attractions;
 
+import people.Visitor;
+
 public class Dodgems extends Attraction {
 
     public Dodgems(String name, int rating) {
         super(name, rating);
+    }
+
+    public double defaultPrice(){
+        return 4.50;
+    }
+
+    public double priceFor(Visitor visitor){
+        if (visitor.getAge() <= 12){return 2.25;}
+        else{return defaultPrice();}
     }
 }
--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,25 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1;
+    Visitor visitor2;
+    Visitor visitor3;
+    Visitor visitor4;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor1 = new Visitor(20, 202.1, 300);
+        visitor2 = new Visitor(25, 130.5, 30);
+        visitor3 = new Visitor(11, 150, 30);
+        visitor4 = new Visitor(20, 180, 80);
     }
 
 
@@ -28,5 +37,15 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void canChargeRegularPrice(){
+        assertEquals(4.50, dodgems.priceFor(visitor1), 0.0);
+    }
+
+    @Test
+    public void canChargeHalfPrice(){
+        assertEquals(2.25, dodgems.priceFor(visitor3), 0.0);
     }
 }

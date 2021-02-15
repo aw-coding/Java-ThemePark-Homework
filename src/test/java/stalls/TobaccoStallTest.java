2e8082e3414c32f2025440ba945888eadfaf2c9f
Andrew Wood <andrewwood90@hotmail.com>
2021-02-15 18:18:12 +0000

    two tobaccostall tests passed

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,15 +2,20 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
+        visitor1 = new Visitor(20, 190.1, 300);
+        visitor2= new Visitor(10, 130.5, 30);
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
     }
 
@@ -27,5 +32,15 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void canTestForAgeAndPass(){
+        assertEquals(true, tobaccoStall.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void canTestForAgeAndFail(){
+        assertEquals(false, tobaccoStall.isAllowedTo(visitor2));
     }
 }

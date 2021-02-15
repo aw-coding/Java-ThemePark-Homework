12d8a304facda507535417b8faec832b075b937d
Andrew Wood <andrewwood90@hotmail.com>
2021-02-15 19:06:45 +0000

    one theme park test passed

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,20 @@
+import behaviours.IReviewed;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<IReviewed> offerings;
+
+    public ThemePark(ArrayList<IReviewed> offerings){
+        this.offerings = new ArrayList<IReviewed>();
+    }
+
+    public int getNumberOfIreviewed(){
+        return offerings.size();
+    }
+
+    public void addNewOffering(IReviewed iReviewed){
+        offerings.add(iReviewed);
+    }
 }
---
+++ src/test/java/ThemeParkTest.java
@@ -0,0 +1,34 @@
+import attractions.Dodgems;
+import behaviours.IReviewed;
+import org.junit.Before;
+import org.junit.Test;
+import stalls.ParkingSpot;
+import stalls.TobaccoStall;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThemeParkTest {
+
+    ThemePark themePark;
+    Dodgems dodgems;
+    TobaccoStall tobaccoStall;
+    ArrayList<IReviewed> offerings;
+
+    @Before
+    public void before() {
+        themePark = new ThemePark(offerings);
+        dodgems = new Dodgems("Fun rides!", 3);
+        tobaccoStall = new TobaccoStall("Smoke Supreme",
+                "Megan Marlboro", ParkingSpot.A1, 4);
+
+    }
+
+    @Test
+    public void canAddOffering(){
+        themePark.addNewOffering(dodgems);
+        assertEquals(1, themePark.getNumberOfIreviewed());
+    }
+
+}
